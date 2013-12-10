b0d54002fd6f8fa04c7cc8e2854a461717504402
Marcell Tóth <tothmarcell97@gmail.com>
2013-12-10 15:23:30 -0600

    initial upload

---
+++ GetImage.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "GetImage.h"
+
+#include "opencv2\imgproc\imgproc.hpp"
+#include "opencv2\highgui\highgui.hpp"
+
+Mat GetImageFromWebcam()
+{
+	//Read the video stream
+	CvCapture* capture = cvCaptureFromCAM(1);
+	IplImage* frame = cvQueryFrame( capture );
+
+	return Mat(frame);
+}
